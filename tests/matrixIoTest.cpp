37a96de75671690f397ac5bdef8d71012924f488
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2023-02-02 17:49:30 +0100

    chore(format): Run clang-format

--- tests/matrixIoTest.cpp
+++ tests/matrixIoTest.cpp
@@ -7,17 +7,17 @@ using namespace Eigen;
 struct MatrixIoFixture {
   MatrixIoFixture()
   {
-    filename = "testmatrix.csv";
-    matrixSize = 3;
+    filename       = "testmatrix.csv";
+    matrixSize     = 3;
     expectedMatrix = MatrixXd(3, 3);
     expectedMatrix << 1, 2, 3,
-                      4, 5, 6,
-                      7, 8, 9;
+        4, 5, 6,
+        7, 8, 9;
   }
 
   std::string filename;
-  int matrixSize;
-  MatrixXd expectedMatrix;
+  int         matrixSize;
+  MatrixXd    expectedMatrix;
 };
 
 BOOST_FIXTURE_TEST_SUITE(MatrixIoTests, MatrixIoFixture)
@@ -28,11 +28,11 @@ BOOST_AUTO_TEST_CASE(OpenData)
 {
   const MatrixXd resultMatrix = openData(filename, matrixSize);
 
-  for(int i = 0; i < matrixSize; i ++) {
+  for (int i = 0; i < matrixSize; i++) {
     BOOST_TEST_CONTEXT("I:" + std::to_string(i))
-    for(int j = 0; j < matrixSize; j ++) {
+    for (int j = 0; j < matrixSize; j++) {
       BOOST_TEST_CONTEXT("J:" + std::to_string(j))
-      BOOST_TEST(expectedMatrix(i,j) == resultMatrix(i,j));
+      BOOST_TEST(expectedMatrix(i, j) == resultMatrix(i, j));
     }
   }
 }
